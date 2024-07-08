f1e9e7333e0d174119187c6851b2305c32f996d8
jason <jason_tan75@hotmail.com>
2024-07-08 10:02:14 -0400

    init commit - added starter code

---
+++ controllers/auth.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+
+const User = require("../models/user.js");
+
+router.get("/sign-up", (req, res) => {
+    res.render("auth/sign-up.ejs");
+});
+
+router.post("/sign-up", async (req, res) => {
+    res.send("Form submission accepted!");
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (userInDatabase) {
+        return res.send("Username already taken.");
+    }
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.send("Password and Confirm Password must match");
+    }
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+    req.body.password = hashedPassword;
+
+    const user = await User.create(req.body);
+    req.session.user = {
+        username: user.username,
+    };
+    req.session.save(() => {
+        res.redirect("/");
+    });
+});
+
+router.get("/sign-in", (req, res) => {
+    res.render("auth/sign-in.ejs");
+});
+
+router.post("/sign-in", async (req, res) => {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (!userInDatabase) {
+        return res.send("Login failed. Please try again.");
+    }
+    const validPassword = bcrypt.compareSync(
+        req.body.password,
+        userInDatabase.password
+    );
+    if (!validPassword) {
+        return res.send("Login failed. Please try again.");
+    }
+    req.session.user = {
+        username: userInDatabase.username,
+    };
+    req.session.save(() => {
+        res.redirect("/");
+    });
+});
+
+router.get("/sign-out", async (req, res) => {
+    req.session.destroy(() => {
+        res.redirect("/");
+    });
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,42 @@
+const dotenv = require("dotenv");
+dotenv.config();
+const express = require("express");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+const morgan = require("morgan");
+const session = require("express-session");
+const MongoStore = require("connect-mongo");
+
+const app = express();
+const port = process.env.PORT ? process.env.PORT : "3000";
+const authController = require("./controllers/auth.js");
+const isSignedIn = require("./middleware/is-signed-in.js");
+const passUserToView = require("./middleware/pass-user-to-view.js");
+
+mongoose.connect(process.env.MONGODB_URI);
+mongoose.connection.on("connected", () => {
+    console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
+});
+
+app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
+app.use(
+    session({
+        secret: process.env.SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGODB_URI,
+        }),
+    })
+);
+app.use(passUserToView);
+app.use("/auth", authController);
+
+app.get("/", (req, res) => {
+    res.render("index.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`The express app is ready on port ${port}!`);
+});
